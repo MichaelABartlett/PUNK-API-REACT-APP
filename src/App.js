09803a10f081ac84f388cae67c30ea3651ffa5ed
unknown <michael@machinistchoice.com>
2021-11-03 16:36:52 -0500

    saving to turn in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import Beers from "./components/Beers"
+
 import './App.css';
 
 let PUNK_API = 'https://api.punkapi.com/v2/beers'
+
+
 
 class App extends React.Component {
 
@@ -11,9 +14,11 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      beers: []
+      beers: [],
+      likedBeers: []
     }
   }
+
 
 // using promises
 // componentDidMount() {
@@ -35,16 +40,33 @@ async componentDidMount() {
 
 componentDidUpdate() {
   console.log( 'This is from state', this.state.beers)
+
 }
+
+
 
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>PUNK API / should be beer API</h1>
-        <Beers beers={this.state.beers} />
+        <ol>{this.state.beers.map((beer, index) => {
+          return (
+            <Beers key={index} name={beer.name} isLiked={false}image_url={beer.image_url} />
+          )
+        })}</ol>
+
+        
       </div>
    );
   }
 }
 
 export default App;
+
+// build function here and add to array
+
+////////4
+//4
+//4
+//4
+//4
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,18 +1,46 @@
 import React from "react";
+import { useState, useEffect } from "react"
+
+let buttonStyle = {
+    marginLeft: '10px',
+    marginRight: '10px',
+    background: 'red'
+}
+
+let buttonStyle2 = {
+    marginLeft: '10px',
+    marginRight: '10px',
+    background: 'green'
+}
+
 
 function Beers(props) {
+
+    const [isLiked, setIsLiked] = useState(false);
+
+    useEffect(() => {
+        console.log(isLiked);
+    }, [isLiked]);
+
+    let handleClick = (props) => {
+        console.log(buttonStyle)
+        setIsLiked(!isLiked);
+      }
+
+
     return(
-        <ul>
-            {props.beers.map((beer, index) => (
-                <li>
-                    <img src={beer.image_url} alt={beer.id}/>
-                    <p>{beer.name}<span><button>Like</button></span></p>
-                   
-                   
-                </li>
-            ))}
-        </ul>
+            <li style={{ listStyle: "none"}}>
+                <img style={{ marginTop: "50px"}} src={props.image_url} alt={props.id}/>
+                <p>{props.name}</p>
+                <button style={isLiked === true ? buttonStyle : buttonStyle2} onClick={handleClick} >
+                  {isLiked === false ? "Liked" : "Not Liked"}  
+                </button>
+            </li>
+            
     )
 }
 
 export default Beers;
+
+// get array value thru props
+
